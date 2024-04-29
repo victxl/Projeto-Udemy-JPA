e3dc7cb5570a82733c79affa13749a2e38a57c5e
Victor Henrique <65699690+victxl@users.noreply.github.com>
2024-04-29 06:22:04 -0300

    Usuario entity e resource

---
+++ src/main/java/com/victxl/curso/resources/ResourcesUsuario.java
@@ -0,0 +1,18 @@
+package com.victxl.curso.resources;
+
+import com.victxl.curso.entities.Usuario;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuario")
+public class ResourcesUsuario {
+    @GetMapping
+    public ResponseEntity<Usuario> findAll(){
+        Usuario usuario = new Usuario(1,"Maria","maria@gmail.com","9999-9999","12345");
+        return ResponseEntity.ok().body(usuario);
+    }
+
+}
