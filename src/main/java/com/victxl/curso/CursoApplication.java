b9046c98363639aae75f0b1086584697d23f1fdc
Victor Henrique <65699690+victxl@users.noreply.github.com>
2024-04-29 06:01:15 -0300

    Criando Projeto

---
+++ src/main/java/com/victxl/curso/CursoApplication.java
@@ -0,0 +1,14 @@
+package com.victxl.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
